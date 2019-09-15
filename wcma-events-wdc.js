c0230e9e5ccbc7e416ab5acae6ab5945239cd1f2
Chad Weinard <cweinard@gmail.com>
2019-09-15 15:38:04 -0400

    added fields

--- wcma-events-wdc.js
+++ wcma-events-wdc.js
@@ -9,10 +9,43 @@
             id: "eventName",
             dataType: tableau.dataTypeEnum.string
         }, {
-            id: "startDate",
+            id: "facultyMember",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "subjectAndCourse",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "subject",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "courseNbr",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "institution",
             dataType: tableau.dataTypeEnum.string
         }, {
             id: "description",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "startDate",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "startYear",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "startMonth",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "startDay",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "dayOfTheWeek",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "terms",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "relatedObjects",
             dataType: tableau.dataTypeEnum.string
         
         }];
@@ -36,8 +69,20 @@
                 tableData.push({
                     "eventId": feat[i].eventId,
                     "eventName": feat[i].eventName,
+                    "facultyMember": feat[i].facultyMember,
+                    "subjectAndCourse": feat[i].subjectAndCourse,
+                    "subject": feat[i].subject,
+                    "courseNbr": feat[i].courseNbr,
+                    "institution": feat[i].institution,
+                    "description": feat[i].description,
                     "startDate": feat[i].startDate,
-                    "description": feat[i].description
+                    "startYear": feat[i].startYear,
+                    "startMonth": feat[i].startMonth,
+                    "startDay": feat[i].startDay,
+                    "dayOfTheWeek": feat[i].dayOfTheWeek,
+                    "terms": feat[i].terms.Term,
+                    "relatedObjects": feat[i].relatedObjects.objectID
+
                 });
             }
     
